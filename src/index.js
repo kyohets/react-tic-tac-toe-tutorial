9e0894285a785ca9e58947223ce4879a19157a32
kyohets <kt.kyohei.tk@gmail.com>
2022-04-24 12:00:08 +0900

    updated to hold all the data in parent Board class

--- src/index.js
+++ src/index.js
@@ -2,19 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={ () => this.props.onClick() }
+//       >
+//         {this.props.value}
+//       </button>
+//     )
+//   }
+// }
+
+// replaced 👆 to 👇 to use function component
+
+function Square(props) {
+  return (
+    <button className='square' onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
+  handleClick(i) {
+    // using 'slice' bacause we want to create a copy of state.squares
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({squares: squares})
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i) }
+      />
+    )
   }
 
   render() {
